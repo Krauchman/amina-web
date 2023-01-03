de69048750b4a66efbc1655bd7bdce91b970021d
Rauan Omarov <rauan.omarov124@gmail.com>
2023-01-04 05:28:10 +0600

    add projects

--- src/components/Home/Projects/Project.js
+++ src/components/Home/Projects/Project.js
@@ -1,4 +1,6 @@
 import "./Project.css";
+import placeholder from './placeholder.png';
+import Cat from './cat.svg';
 
 export function Project(props) {
     const name = props.name
@@ -7,8 +9,29 @@ export function Project(props) {
     return (
         <div className="Project-container">
             <div className="Project">
-                {name}
-                {description}
+                <div className="Project-text-container">
+                    <div className="Project-name">
+                        {name}
+                    </div>
+                    <div className="Project-description">
+                        {description}
+                    </div>
+                </div>
+                <div className="Project-image-container">
+                    <img className="Project-image" alt={name} src={placeholder} />
+                </div>
+
+                {/* ON HOVER */}
+                <div className="Hover">
+                    <div className="Hover-name">
+                        {name}
+                    </div>
+                    <div className="Hover-link">
+                        View Full Project
+                    </div>
+                    <img src={Cat} className="Hover-cat" alt="meow" />
+                </div>
+
             </div>
         </div>
     );
