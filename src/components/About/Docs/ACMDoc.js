6325d560bf39b0cb86a572c98540ab6c8f635f13
Rauan Omarov <rauan.omarov124@gmail.com>
2023-01-08 18:47:57 +0600

    start photocards

--- src/components/About/Docs/ACMDoc.js
+++ src/components/About/Docs/ACMDoc.js
@@ -1,5 +1,6 @@
 import "./index.css";
 import ACMHeader from "./photos/acm_header.png"
+import { PhotoCard } from "./PhotoCard";
 
 export function ACMDoc() {
     return (
@@ -14,11 +15,19 @@ export function ACMDoc() {
                 </div>
 
                 <div className="Doc-cell Doc-body">
-                    <h1>ACM & ACM-W Student Chapters: from designer to hackathon organizer</h1>
 
-                    <p>ACM & ACM-W Student Chapters are academic clubs intended to create opportunities for young IT community at our University. We organize all kinds of events that motivate students interested in programming, ranging from coding marathons, hackathons to networking sessions and movie nights. We were honored “Best Academic Club” status several times among 200 student clubs at our university. Apart from this, we encourage female students in tech by organizing coding competitions and guest talks solely for them. As such, number of female participants in our events is increasing each year by almost 30%!</p>
+                    <h1 className="block">ACM & ACM-W Student Chapters: from designer to hackathon organizer</h1>
 
-                    <p>I joined ACM & ACM-W Student Chapter in 2020 as a graphic designer who produced visuals for social media and design for merchandise. There were no design guidelines so I set up my own by choosing color palette, fonts and style which is still being polished. This helped a lot for me and other designers to maintain consistency among visuals we created (this is especially noticeable in our Instagram page). Our event posters and social media posts became more recognizable by other students (they keep asking us if we hired graphic designer who’s creating all these designs).</p>
+                    <p className="block">ACM & ACM-W Student Chapters are academic clubs intended to create opportunities for young IT community at our University. We organize all kinds of events that motivate students interested in programming, ranging from coding marathons, hackathons to networking sessions and movie nights. We were honored “Best Academic Club” status several times among 200 student clubs at our university. Apart from this, we encourage female students in tech by organizing coding competitions and guest talks solely for them. As such, number of female participants in our events is increasing each year by almost 30%!</p>
+
+                    <p className="block">I joined ACM & ACM-W Student Chapter in 2020 as a graphic designer who produced visuals for social media and design for merchandise. There were no design guidelines so I set up my own by choosing color palette, fonts and style which is still being polished. This helped a lot for me and other designers to maintain consistency among visuals we created (this is especially noticeable in our Instagram page). Our event posters and social media posts became more recognizable by other students (they keep asking us if we hired graphic designer who’s creating all these designs).</p>
+
+                    <div className="block Doc-photo-block">
+                        <PhotoCard />
+                        <PhotoCard />
+                        <PhotoCard />
+                    </div>
+
                 </div>
 
             </div>
---
+++ src/components/About/Docs/PhotoCard.js
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export function PhotoCard(props) {
+    return (
+        <div className="Photo-Card">
+            PhotoCard
+        </div>
+    );
+}
