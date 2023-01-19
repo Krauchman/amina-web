30d98b6fe61adadede882caddb20a95ba9d45d99
Rauan Omarov <rauan.omarov124@gmail.com>
2023-01-19 11:54:58 +0600

    improve lazy image animations

--- src/components/Image/LazyImage.js
+++ src/components/Image/LazyImage.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 export function LazyImage({ src, alt, placeholder, className }) {
     const [isLoading, setIsLoading] = useState(true);
     const [isPlaceholderLoading, setIsPlaceholderLoading] = useState(true);
+    const [showImage, setShowImage] = useState(false);
 
     const onImageLoad = () => {
         setIsLoading(false);
@@ -14,19 +15,35 @@ export function LazyImage({ src, alt, placeholder, className }) {
         setIsPlaceholderLoading(false);
     }
 
+    const onPlaceholderAnimationEnd = (event) => {
+        if (event.animationName === "placeholder-appear") {
+            setShowImage(true);
+        }
+    }
+
     let placeholderClassName = className + " lazy-image placeholder";
     if (isPlaceholderLoading) {
         placeholderClassName += " loading";
     }
 
     let imgClassName = className + " lazy-image original";
-    if (isLoading) {
+    if (isLoading || !showImage) {
         imgClassName += " loading";
     }
 
     return (
         <>
-            {isLoading && <img src={placeholder} alt={alt} onLoad={onPlaceholderLoad} className={placeholderClassName} />}
+            {
+                (isLoading || !showImage)
+                &&
+                <img
+                    src={placeholder}
+                    alt={alt}
+                    onLoad={onPlaceholderLoad}
+                    className={placeholderClassName}
+                    onAnimationEnd={onPlaceholderAnimationEnd}
+                />
+            }
             <img src={src} alt={alt} onLoad={onImageLoad} className={imgClassName} />
         </>
     )
