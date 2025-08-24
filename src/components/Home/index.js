e8a2959d967897066f6497f9d9d56ec02a4a5f62
Rauan Omarov <krauch@google.com>
2025-08-24 19:19:38 +0200

    home layout done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,6 +12,11 @@ import LinkArrowBlack from './media/link-arrow-black.svg';
 import Project1 from './media/project1.png';
 import Project2 from './media/project2.png';
 
+import HeroDecor from './media/hero-decor.png';
+
+import FooterPhoto from './media/footer-photo.png';
+import StatusDot from './media/status-dot.svg';
+
 export function Home() {
     return (
         <div>
@@ -69,7 +74,6 @@ export function Home() {
                             <img src={LinkArrowBlack} alt="arrow" className="arrow" />
                         </div>
                     </div>
-
                     <div className="v2-block padding-2xs project project2">
                         <img src={Project2} alt="Cashbacks" />
                         <div className="text">
@@ -81,8 +85,65 @@ export function Home() {
                         </div>
                     </div>
 
-                    <div className="v2-block hero">1</div>
-                    <div className="v2-block footer">1</div>
+
+                    <div className="v2-block hero">
+                        <div className="summary">
+                            Alongside the core values I believe every product
+                            designer should strive for—<b>craft</b>, <b>clarity</b>, and
+                            <b> collaboration</b>—I also try to bring qualities that I’ve
+                            seen elevate good design into great products
+                        </div>
+                        <div className="decor-conatiner">
+                            <img src={HeroDecor} alt="decor" className="decor" />
+                        </div>
+                        <div className="skills">
+                            <div className="skill">
+                                <div className="name">Ownership</div>
+                                <div className="description">
+                                    I take initiative and stay accountable
+                                    from discovery to delivery
+                                </div>
+                            </div>
+                            <div className="skill">
+                                <div className="name">Impact</div>
+                                <div className="description">
+                                    I design with purpose, always tying
+                                    decisions to business value
+                                </div>
+                            </div>
+                            <div className="skill">
+                                <div className="name">Systems</div>
+                                <div className="description">
+                                    I think in patterns, not pages,
+                                    creating consistent experiences
+                                </div>
+                            </div>
+                            <div className="skill">
+                                <div className="name">Transparency</div>
+                                <div className="description">
+                                    I communicate openly and bring
+                                    clarity to every stage of the process
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="v2-block footer">
+                        <div className="status">
+                            Open to new
+                            <span className="v2-no-wrap-group">
+                                opportunities <img src={StatusDot} alt="status" />
+                            </span>
+                        </div>
+                        <div className="contacts">
+                            <a className="v2-link" target="_blank" href="mailto:alenamina1010@gmail.com">alenamina1010@gmail.com</a>
+                            <a className="v2-link" target="_blank" href="https://www.linkedin.com/in/aminaalen/">LinkedIn</a>
+                        </div>
+                        <div className="photo-container">
+                            <img src={FooterPhoto} alt="my photo" />
+                        </div>
+                    </div>
 
                 </div>
             </div>
